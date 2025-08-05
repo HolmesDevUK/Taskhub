8e1110ddfb39b1d37c1afc5aac70b28fe96bdfc2
HolmesDev <holmesdevuk@gmail.com>
2025-08-05 18:50:24 +0100

    add ClientLoginView

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+
+from .forms import ClientLoginForm
+
+class ClientLoginView(LoginView):
+    authentication_form = ClientLoginForm
+    template_name = "accounts/login.html"
 
 # Create your views here.
