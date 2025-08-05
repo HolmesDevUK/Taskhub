6778bc402cc81efc8fc655632586aa5007d062ad
HolmesDev <holmesdevuk@gmail.com>
2025-08-05 14:47:17 +0100

    add CustomUser

--- accounts/models.py
+++ accounts/models.py
@@ -13,7 +13,21 @@ class CustomUserManager(BaseUserManager):
         user.save()
         return user
     
-    def create_superuser(self, email, password=None, **extra_fields)
+    def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         return self.create_user(email, password, **extra_fields)
+    
+class CustomUser(AbstractBaseUser, PermissionError):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=225)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name"]
+
+    def __str__(self):
+        return self.email    
