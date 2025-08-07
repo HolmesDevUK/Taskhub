59b8e84e8de249d6b5a8c55e50b824eefa927f4d
HolmesDev <holmesdevuk@gmail.com>
2025-08-07 15:15:17 +0100

    add admin_base.html and client_base.html

--- clients/views.py
+++ clients/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from core.mixins import ClientRequiredMixin
+from django.views.generic import ListView
 
 # Create your views here.
