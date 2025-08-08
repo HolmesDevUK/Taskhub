eaaf427edebb3e6a324c873515bceb8b30f13540
HolmesDev <holmesdevuk@gmail.com>
2025-08-08 11:36:31 +0100

    add createclientform

---
+++ adminpanel/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from accounts.models import CustomUser
+
+class CreateClientForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ["email", "name"]
+
