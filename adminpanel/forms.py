d2820c4257e363065a668bb0e499de3f6c815021
HolmesDev <holmesdevuk@gmail.com>
2025-08-09 15:40:13 +0100

    create Task form

--- adminpanel/forms.py
+++ adminpanel/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 from accounts.models import CustomUser
+from core.models import Task
 
 class CreateClientForm(forms.ModelForm):
     class Meta:
         model = CustomUser
         fields = ["email", "name"]
+
+class CreateTaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ["title", "description", "client", "deadline", "file", ]        
 
 
