d0ce99eb6b378fb516c459edd39cef06d4b94e0b
HolmesDev <holmesdevuk@gmail.com>
2025-08-09 19:30:21 +0100

    add createtaskview to urls

--- adminpanel/urls.py
+++ adminpanel/urls.py
@@ -5,5 +5,6 @@ app_name = "adminpanel"
 
 urlpatterns = [
      path("dashboard/", views.DashboardView.as_view(), name="dashboard"),
-     path("create-client/", views.CreateClientView.as_view(), name="create_client")
+     path("create-client/", views.CreateClientView.as_view(), name="create_client"),
+     path("create-task/", views.CreateTaskView.as_view(), name="create_task"),
 ]
